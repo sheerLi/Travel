d8f6b53cf48e6d18fea5d30c7ae0d4a734db7a2e
ldx <m17805850283@163.com>
2019-01-12 13:47:40 +0800

    project init

---
+++ src/main.js
@@ -0,0 +1,24 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+
+// 导入初始化样式
+import './assets/styles/reset.css'
+// 解决移动端 1px 样式问题
+import './assets/styles/border.css'
+// 解决移动端点击 300ms 延迟问题
+import fastclick from 'fastclick'
+
+fastclick.attach(document.body)
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
---
+++ src/pages/home/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    home主页
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home'
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
